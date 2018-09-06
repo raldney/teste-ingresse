ea9bb9f923ae849372049cfa5be19078f148d882
Raldney Alves <raldney.alves@gmail.com>
2018-09-06 17:55:53 -0300

    adjust migrate to make correct way

--- database/migrations/2018_09_06_035557_create_users_table.php
+++ database/migrations/2018_09_06_035557_create_users_table.php
@@ -16,11 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();;
             $table->string('password');
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
-            $table->timestamp('deleted_at');
+            $table->timestamps();
+            $table->softDeletes(); 
+            ;
         });
     }
 
